630056496668621dbcf767e978e083143c729dd5
srijankrs <srijankrsamanta@gmail.com>
2020-02-02 01:39:21 +0530

    first commit

---
+++ src/main/java/com/example/barteringandtrading/BarteringandtradingApplication.java
@@ -0,0 +1,13 @@
+package com.example.barteringandtrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarteringandtradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarteringandtradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barteringandtrading/BarteringandtradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barteringandtrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarteringandtradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
