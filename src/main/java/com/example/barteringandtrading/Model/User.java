27d95eac2c849ef72000df7fbd3b2027ddbb808f
SIRIONLABS\kanhaiya.saini <kanhaiya.saini@sirionlabs.com>
2020-02-02 17:20:15 +0530

    add config file

--- src/main/java/com/example/barteringandtrading/Model/User.java
+++ src/main/java/com/example/barteringandtrading/Model/User.java
@@ -3,15 +3,15 @@ package com.example.barteringandtrading.Model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
 @Table(name="user")
 public class User {
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String name;
     private String email;
